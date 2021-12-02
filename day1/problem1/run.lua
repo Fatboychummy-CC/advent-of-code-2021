fc604a08e661518c30988f70052d3e6bbc78aed8
Matthew W <fatboychummy@gmail.com>
2021-12-01 21:54:53 -0700

    commetn

--- day1/problem1/run.lua
+++ day1/problem1/run.lua
@@ -6,11 +6,14 @@ print("Checking", data.n, "entries.")
 local last = math.huge
 local count = 0
 
+-- for each line
 for i = 1, data.n do
+  -- check if the value is larger than the previous.
   if last < tonumber(data[i]) then
     count = count + 1
   end
 
+  -- assign the previous to the current
   last = tonumber(data[i])
 end
 
