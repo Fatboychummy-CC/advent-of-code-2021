cd02908458aec0334182c82b4779310738443a4e
Matthew W <fatboychummy@gmail.com>
2021-12-03 23:26:43 -0700

    Think I've got it.

---
+++ day4/problem1/run.lua
@@ -0,0 +1,186 @@
+-- Ensure memory is cleared.
+new()
+
+-- load the input data from file.
+local data = input()
+
+-- This is purely for debugging purposes to ensure I do stuff correctly.
+local _MARKER = {}
+local function checkself(self)
+  if type(self) ~= "table" or self._MARKER ~= _MARKER then
+    error("Expected ':' when making method call.", 3)
+  end
+end
+
+--[[
+    We'll create "Board" objects which have some helpful methods to check if wins occur when they change.
+    The board is instantiated from the given line as `inputStart`
+
+]]
+local function newBoard(inputStart)
+  -- Load all of the data from the game board.
+  local boardData = {}
+
+  -- This lookup table will be a hashmap of all numbers in the matrix, reducing search time for the positions.
+  local lookup = {}
+
+  for i = 0, 4 do
+    local line = {}
+    local n = 0
+
+    -- There is no verification to be done -- unlike in real bingo, numbers can be in any slot I guess.
+    -- However, it is assumed no number will be given twice
+    for id in data[inputStart + i]:gmatch("%d+") do
+      -- increment n
+      n = n + 1
+
+      -- store whether this position is marked and the number.
+      line[n] = {false, tonumber(id)}
+
+      -- add to the lookup table.
+      lookup[tonumber(id)] = {n, i + 1}
+    end
+
+    -- Add the line of data
+    boardData[i + 1] = line
+  end
+
+  return {
+    _MARKER = _MARKER, -- again, debugging purposes
+    board = boardData,
+    lookup = lookup,
+
+    -- Check method will check if this board has won, given a change at x,y position.
+    Check = function(self, x, y)
+      checkself(self)
+      local sum = 0
+
+      -- check horizontal
+      local horiWon = true
+      for i = 1, 5 do
+        if not self.board[y][i][1] then
+          horiWon = false
+          break
+        end
+      end
+
+      -- If we won, compute sum.
+      if horiWon then
+        for i = 1, 5 do
+          sum = sum + self.board[y][i][2]
+        end
+
+        return true, sum
+      end
+
+      -- check vertical
+      local vertiWon = true
+      for i = 1, 5 do
+        if not self.board[i][x][1] then
+          vertiWon = false
+          break
+        end
+      end
+
+      -- If we won, compute sum.
+      if vertiWon then
+        for i = 1, 5 do
+          sum = sum + self.board[i][x][2]
+        end
+
+        return true, sum
+      end
+    end,
+
+    -- Mark method will mark
+    Mark = function(self, n)
+      checkself(self)
+
+      -- if this number is in our board.
+      if self.lookup[n] then
+        -- mark it
+        local x, y = table.unpack(self.lookup[n], 1, 2)
+        if not self.board[y] or not self.board[y][x] then
+          print(textutils.serialize(self.lookup[n]))
+          error(string.format("board missing datapoints %s %s from lookup.", y, x), 2)
+        end
+        self.board[y][x][1] = true
+
+        -- then check if that made us win.
+        return self:Check(x, y)
+      end
+
+      -- can't win if nothing changed...
+      return false
+    end,
+
+    -- Clear method will set the entire board to false.
+    Clear = function(self)
+      checkself(self)
+
+      for y = 1, 5 do
+        for x = 1, 5 do
+          self.board[y][x] = false
+        end
+      end
+    end
+  }
+end
+
+--[[
+    We will read the input data into game boards, and an order of numbers.
+]]
+local function getGameData()
+  -- read the order of numbers into memory.
+  local nums = {n = 0}
+  for num in data[1]:gmatch("%d+") do
+    nums.n = nums.n + 1
+    nums[nums.n] = tonumber(num)
+  end
+
+  -- read all the gameboards into memory.
+  local boards = {n = 0}
+  for i = 3, data.n, 6 do
+    boards.n = boards.n + 1
+    boards[boards.n] = newBoard(i)
+  end
+
+  return {nums = nums, boards = boards}
+end
+
+--[[
+    We will run the game, until one board wins.
+    We will return the sum of winning line and last number.
+]]
+local function runGame()
+  local game = getGameData()
+
+  -- for each number in the input.
+  for i = 1, game.nums.n do
+    local nextNumber = game.nums[i]
+
+    -- for each board in the game.
+    for i = 1, game.boards.n do
+      -- mark the number on the board
+      local boardHasWon, lineSum = game.boards[i]:Mark(nextNumber)
+
+      -- and check if it won
+      if boardHasWon then
+        return lineSum, nextNumber, i
+      end
+    end
+  end
+
+  error("End of input reached but no board has won!", 0)
+end
+
+local sum, num, boardNumber = runGame()
+
+print(string.format("Board #%d has won, with line sum of %d. The last number called was %d.", boardNumber, sum, num))
+print(string.format("The winning score is: %d", sum * num))
+
+-- mark this data to be written to file (for easy copy/paste)
+output(tostring(sum * num))
+
+-- write the output file.
+finalize()
